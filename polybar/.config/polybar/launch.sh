5bc894fa26e7a3f408b1f45aabf2a3d920317fc8
northkill <northkillz@gmail.com>
2020-01-30 09:18:24 +0900

    Tweak polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
 # Bar is the name set in the polybar config, so if you change it, you have to change it here too.
-MONITOR="HDMI-3" polybar bar &
-MONITOR="HDMI-1-2" polybar bar &
+MONITOR="HDMI-3" polybar left &
+MONITOR="HDMI-1-2" polybar right &
 
 echo "Bars launched..."
