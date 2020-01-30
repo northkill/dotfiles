4b99fc9f6aba006882236d349171a359d5da2e27
northkill <northkillz@gmail.com>
2020-01-30 11:05:10 +0900

    Add bottom left polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar
 # Bar is the name set in the polybar config, so if you change it, you have to change it here too.
 MONITOR="HDMI-3" polybar left &
+MONITOR="HDMI-3" polybar leftbottom &
 MONITOR="HDMI-1-2" polybar right &
 
 echo "Bars launched..."
